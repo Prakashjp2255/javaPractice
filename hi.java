ce60cf98707ca0b388ed836521725672e23bd3b6
prakashjp2255 <jayaprakashm21cb@psnacet.edu.in>
2025-08-03 20:59:58 +0530

    Initial changes

--- hi.java
+++ hi.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class hi {
     public static void main(String[] args) {
         System.out.println("hello world ");
+        System.out.println("Enter a amount :");
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
     }
 }
